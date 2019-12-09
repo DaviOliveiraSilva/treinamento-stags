903ead126570d42c77d18165348383207c8ba8b2
Davi Olvieira <davicampinas@gmail.com>
2019-12-09 17:17:40 -0300

    WIP | setup

---
+++ src/load.js
@@ -0,0 +1,13 @@
+var pokemons = [];
+
+async function load() {
+    const x = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+    let y = await x.json();
+    for (let i = 0; i < y.results.length; i++) {
+        const element = y.results[i];
+        const request = await fetch(element.url)
+        let pokemon = await request.json();
+    
+        pokemons[i] = pokemon
+    }
+}
--- src/script.js
+++
@@ -1,5 +0,0 @@
-
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(data => {return data.json()})
-    .then(response => console.log(response))
-    .catch(error => console.error(error))
