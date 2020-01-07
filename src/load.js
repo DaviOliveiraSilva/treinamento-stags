310f3a3f007f8157c57897fba68c96194b60b9cd
Davi Olvieira <davicampinas@gmail.com>
2020-01-07 08:43:59 -0300

    Started my version

--- src/load.js
+++ src/load.js
@@ -1,11 +1,13 @@
 async function loadPokemons() {
-    const x = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
-    let y = await x.json();
-    for (let i = 0; i < y.results.length; i++) {
-        const element = y.results[i];
-        const request = await fetch(element.url)
-        let pokemon = await request.json();
-    
-        pokemons[i] = pokemon
+    if(true){
+        const x = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
+        let y = await x.json();
+        for (let i = 0; i < y.results.length; i++) {
+            const element = y.results[i];
+            const request = await fetch(element.url)
+            let pokemon = await request.json();
+        
+            pokemons[i] = pokemon
+        }
     }
 }
