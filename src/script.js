04a1b98072370ddedead4616a730ddfc76291021
Davi Olvieira <davicampinas@gmail.com>
2020-01-07 08:36:55 -0300

    Script de troca de páginas

--- src/script.js
+++ src/script.js
@@ -1,4 +1,27 @@
 var pokemons = [];
 loadPokemons().then(() => {
-    console.log(pokemons)
+    console.log(pokemons);
+    open('home')
 })
+
+function test() {
+    console.log('deu bom');
+}
+
+function open(page){
+    var rawFile = new XMLHttpRequest();
+    rawFile.open('GET', `pages/${page}.html`, true);
+    rawFile.setRequestHeader('Content-Type', 'application/xml');
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                document.getElementById("container").innerHTML = allText
+            }
+        }
+    }
+    rawFile.send();
+}
