6caea6e88acc145ce99801b838316f99c99b4cfc
Davi Olvieira <davicampinas@gmail.com>
2020-01-20 13:41:05 -0300

    WIP

--- src/script.js
+++ src/script.js
@@ -1,27 +1,3 @@
 loadPokemons().then(() => {
-    console.log(pokemons);
+    
 })
-
-const openPage = () =>{
-    const local = location.hash.slice(2);
-    
-    var rawFile = new XMLHttpRequest();
-    rawFile.open('GET', `pages/${local}.html`, true);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                document.getElementById("content").innerHTML = allText
-            }
-        }
-    }
-    rawFile.send();
-    
-
-}
-
-window.addEventListener('hashchange', openPage)
-window.addEventListener('load', openPage)
